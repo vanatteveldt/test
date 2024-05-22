9b1ee02cab0aab43988c551249fc3522dcfc2c13
Wouter van Atteveldt <wouter@vanatteveldt.com>
2024-05-22 14:05:11 +1000

    Topics from yaml

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-import datetime
-with open("test.txt", "w") as f:
-    f.write(datetime.datetime.now().isoformat())
+import yaml
+
+
+
+def generate_md(cb, lang='en'):
+    def x(item):
+        if item is None:
+            return "-"
+        if isinstance(item, str):
+            return item
+        return item.get(lang, item.get('en'))
+    for topic, d in i.items():
+        description = x(d.get('description'))
+        yield f"## {topic}: {description}"
+        if 'positive' in d:
+            yield f'Positions in favour include: {x(d["positive"])}'
+        if 'examples' in d:
+            examples = "\n".join(f"+ {x(ex)}" for ex in d['examples'])
+            yield f"#### Examples:\n {examples}"
+
+i = yaml.safe_load(open("topics.yml"))
+for lang in ['en', 'nl']:
+    md = "\n\n".join(generate_md(i))
+    open(f"topics-{lang}.md", "w").write(md)
---
+++ topics-nl.md
@@ -0,0 +1,17 @@
+## Defense: Defense, investments in the army, navy, air force or military equipment.
+
+Positions in favour include: Strong defense/NATO; investment in Defense, a stronger of more effective army, and achieving the NATO goal of 2% for the national gross product for Defense.
+
+#### Examples:
+ + VVD in favor of defence expenditures of 2% of GDP
+
+## Agriculture: Farming, farmers, agricultural output, cattle, nitrogen as a result of farming activity
+
+Positions in favour include: Maintaining (intensive) argigulture
+
+#### Examples:
+ + BBB: the D66 proposal for nitrogen reduction is a disaster for our farmers
+
+## Bla: -
+
+Positions in favour include: ?
